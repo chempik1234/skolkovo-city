90d2db5263343302746f3c2e7eda691b4b178c4b
Danis <dandijar@yandex.ru>
2025-07-16 15:30:38 +0500

    feat(admin): category form: image urls = "" if None

--- skolkovo_admin/main_app/forms.py
+++ skolkovo_admin/main_app/forms.py
@@ -5,5 +5,7 @@ from django.utils.safestring import mark_safe
 
 class ImageArrayWidget(forms.Textarea):
     def render(self, name, value, attrs=None, renderer=None):
+        if not value:
+            value = ""
         value = value.replace(',', ',\n\n')
         return mark_safe(f'<textarea style="width: 700px; height: 200px" name="{escape(name)}">{escape(value)}</textarea>')
