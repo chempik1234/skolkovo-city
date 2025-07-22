89168974f8b65aa503945dc74e971be0fa9c00ce
Danis <dandijar@yandex.ru>
2025-07-22 14:47:50 +0500

    feat(bot): get time till next weather & weather text in main menu

--- bot/bot_functions/category.py
+++ bot/bot_functions/category.py
@@ -5,7 +5,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, InputMediaPhoto, InputMediaVideo, InputMedia, URLInputFile
 
 from config import States
-from init import bot, category_service
+from init import bot, category_service, weather_service
 from init_configs import bot_config
 from keyboards import category_keyboard
 from middlewares.prometheus import track_category_click_async
@@ -59,6 +59,13 @@ async def send_category(category_message: Message | None, chat_id: int | str | N
 
     # erase existing media messages
     asyncio.create_task(delete_media_messages_from_state(state, send_to))
+
+    # append weather to text
+    if isinstance(category, CategoryModel) and str(category.id) == bot_config.BOT_ROOT_CATEGORY_STR:
+        weather_text = await weather_service.get_weather_text(language)
+        if weather_text:
+            text = "/n/n".join([weather_text, text])
+
     await state.update_data({"media_messages": []})
 
     if category is None:
